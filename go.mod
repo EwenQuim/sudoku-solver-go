137373029653b20f44be71e5115a6b865aaa40c8
EwenQuim <ewen.quimerch@gmail.com>
2021-02-03 19:49:48 +0100

    🎨 pretty CLI

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module sudoku-solver
 
 go 1.15
 
-require github.com/pkg/profile v1.5.0
+require (
+	github.com/fatih/color v1.10.0
+	github.com/pkg/profile v1.5.0
+)
--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-
-	"github.com/pkg/profile"
-)
-
 func main() {
 
-	var profiling = flag.Bool(
-		"cpu",
-		false,
-		"Profiling CPU usage",
-	)
-	flag.Parse()
-
-	flag.Args()
-
-	if *profiling {
-		defer profile.Start().Stop()
-	}
+	sudokus := PlugCLI()
 
 	// multiple sudokus can be given
-	for _, sudokuFile := range flag.Args() {
-		fmt.Println("\nFile:", sudokuFile)
+	for _, sudokuFile := range sudokus {
+		PrettyTitle(sudokuFile)
 
 		S := FileHandler(sudokuFile)
 
@@ -33,11 +15,4 @@ func main() {
 		PrettyPrint(T)
 	}
 
-}
-
-// PrettyPrint prints matrix
-func PrettyPrint(S [9][9]uint8) {
-	for i := 0; i < 9; i++ {
-		fmt.Println(S[i])
-	}
 }
